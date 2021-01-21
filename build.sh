7d4fefa2ebe3e2eaa083049ba31c92d8b2b29f3c
bushraasif931 <bua@embrace-it.com>
2021-01-21 18:51:30 +0500

    Remove sdk installtion script from build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,6 @@
 
 if command -v dpkg-query -l zip
 then
-  git clone --branch 3.0.1 https://github.com/AltaPay/sdk-php.git
-  cp -rf sdk-php/lib/* lib/altapay/altapay-php-sdk/lib/
-  rm -rf sdk-php/
   composer install -a -o --no-dev
   find . -type d -exec cp index.php {} \;
   mkdir -p dist/altapay
@@ -12,7 +9,6 @@ then
   cd ./dist
   zip altapay.zip -r altapay
   rm -r altapay
-  cd ../lib/altapay/altapay-php-sdk/lib/
   find . \! -name 'helpers.php' -delete
 else
   echo "Zip package is not currently installed"
