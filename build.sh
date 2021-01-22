89b9976775eb94de061a796e4b99ad0a58b448ed
bushraasif931 <bua@embrace-it.com>
2021-01-22 14:15:55 +0500

    Remove unused code and update composer

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@
 if command -v dpkg-query -l zip
 then
   composer install -a -o --no-dev
+  composer isolate
   find . -type d -exec cp index.php {} \;
   mkdir -p dist/altapay
   rsync -rv --exclude=build.sh --exclude=.gitignore --exclude=guide.md * dist/altapay
   cd ./dist
   zip altapay.zip -r altapay
-  rm -r altapay
-  find . \! -name 'helpers.php' -delete
+  rm -r altapay ../vendor
 else
   echo "Zip package is not currently installed"
 fi
