fb292a4f471c3c99026eaa4b0bc7f3218a64c234
bushraasif931 <bua@embrace-it.com>
2021-01-28 00:38:33 +0500

    Update bash script

--- build.sh
+++ build.sh
@@ -2,14 +2,32 @@
 
 if command -v dpkg-query -l zip
 then
-  composer install -a -o --no-dev
+  set -e
+
+  if ! command -v zip
+  then
+    echo "Zip package is not currently installed"
+    exit
+  fi
+
+  if ! command -v php7.0
+  then
+    echo "PHP 7.0 package is not currently installed"
+    exit
+  fi
+
+  if ! command -v composer
+  then
+    echo "Composer package is not currently installed"
+    exit
+  fi
+
+  php7.0 $(command -v composer) install --no-dev -o
   composer isolate
+  composer dump -a
   find . -type d -exec cp index.php {} \;
   mkdir -p dist/altapay
-  rsync -rv --exclude=build.sh --exclude=.gitignore --exclude=guide.md * dist/altapay
-  cd ./dist
-  zip altapay.zip -r altapay
-  rm -r altapay ../vendor
+  zip dist/altapay.zip -r * -x "dist/*" build.sh guide.md .gitignore phpunit.xml.dist phpstan.neon.dist phpstan.neon composer.json composer.lock @
 else
   echo "Zip package is not currently installed"
 fi
