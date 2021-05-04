5823d8687e0a274a7bb0a720ca0b73d1cabbeae2
Muhammad Idrees <saadidrees57@gmail.com>
2021-05-05 02:18:57 +0500

    Integration tests for Prestashop 1.6

---
+++ Test/Integration Test/cypress/integration/examples/payment.js
@@ -0,0 +1,29 @@
+import Order from '../PageObjects/objects'
+
+describe ('Presta 1.6', function(){
+
+    it('CC Payment', function(){
+
+        const ord = new Order()
+        ord.clrcookies()
+        ord.visit()
+        ord.signin()
+        ord.addproduct()
+        ord.cc_payment()
+        ord.admin()
+        ord.capture()
+    })
+
+    it('klarna', function(){
+
+        const ord = new Order()
+        ord.clrcookies()
+        ord.visit()
+        ord.signin()
+        ord.addproduct()
+        ord.klarna_payment()
+        ord.admin()
+        ord.capture()
+    })
+
+})
