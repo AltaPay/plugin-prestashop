f558a184f19d37d3d013383b546d32c98a7289b0
Bushra Asif <bushraasif931@gmail.com>
2021-06-14 15:00:08 +0500

    Handle fail statuses properly (#30)
    * Handle fail statuses properly
    
    * Minor update
    
    * Update code anylsis script
    
    * Update code-analysis script
    
    * Changelog updated
    
    * Update changelog

--- helpers.php
+++ helpers.php
@@ -24,7 +24,7 @@
 function transactionInfo($transactionInfo = [])
 {
     $pluginName = 'altapay';
-    $pluginVersion = '3.3.4';
+    $pluginVersion = '3.3.5';
 
     // Transaction info
     $transactionInfo['ecomPlatform'] = 'PrestaShop';
