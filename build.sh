ea86ff39f7f80de9d1488f43595a03c8f609c282
Bushra Asif <bushraasif931@gmail.com>
2022-12-21 10:24:35 +0500

    Build package with php 7.2

--- build.sh
+++ build.sh
@@ -10,9 +10,9 @@ then
     exit
   fi
 
-  if ! command -v php7.4
+  if ! command -v php7.2
   then
-    echo "PHP 7.4 package is not currently installed"
+    echo "PHP 7.2 package is not currently installed"
     exit
   fi
 
@@ -22,12 +22,12 @@ then
     exit
   fi
 
-  php7.4 $(command -v composer) install --no-dev --no-interaction
-  mkdir -p package && php7.4 vendor/bin/php-scoper add-prefix --output-dir ./package
-  rm -rf vendor/* && mv package/vendor/* vendor/
+  php7.2 $(command -v composer) install --no-dev --no-interaction
+  mkdir -p package && yes | php7.2 vendor/bin/php-scoper add-prefix --output-dir ./package
+  rsync -a package/vendor/* vendor/
   find . -type d -exec cp index.php {} \;
   mkdir -p dist/altapay && rsync -av --exclude={'package','dist','docker','Docs','build.sh','guide.md','.gitignore','phpstan.neon'} * dist/altapay
-  cd dist/altapay/ && php7.4 $(command -v composer) dump-autoload --working-dir ./ --classmap-authoritative
+  cd dist/altapay/ && php7.2 $(command -v composer) dump-autoload --working-dir ./ --classmap-authoritative
   cd ../ && zip altapay.zip -r *
 else
   echo "Zip package is not currently installed"
