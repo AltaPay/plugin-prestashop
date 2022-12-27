62fad5f51c5d2b5441334be9716ee46fad94d414
Bushra Asif <bushraasif931@gmail.com>
2022-12-27 11:12:54 +0500

    Update build script

--- build.sh
+++ build.sh
@@ -23,10 +23,10 @@ then
   fi
 
   php7.2 $(command -v composer) install --no-dev --no-interaction
-  mkdir -p package && yes | php7.2 vendor/bin/php-scoper add-prefix --output-dir ./package
-  rsync -a package/vendor/* vendor/ && rm -rf build/
+  mkdir -p package && yes | php7.2 vendor/bin/php-scoper add-prefix
+  rsync -a build/vendor/* vendor/ && rm -rf build/
   find . -type d -exec cp index.php {} \;
-  mkdir -p dist/altapay && rsync -av --exclude={'package','dist','docker','Docs','build.sh','guide.md','.gitignore','phpstan.neon'} * dist/altapay
+  mkdir -p dist/altapay && rsync -av --exclude={'build','dist','docker','Docs','build.sh','guide.md','.gitignore','phpstan.neon'} * dist/altapay
   cd dist/altapay/ && php7.2 $(command -v composer) dump-autoload --working-dir ./ --classmap-authoritative
   cd ../ && zip altapay.zip -r *
 else
