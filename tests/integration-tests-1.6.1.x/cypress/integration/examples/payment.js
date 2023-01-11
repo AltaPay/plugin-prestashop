213c3885931a768137b1ab4e3a579195f82e6679
Taimur <taimurmansur555@gmail.com>
2023-01-11 17:52:46 +0500

    Enable ideal tests

--- tests/integration-tests-1.6.1.x/cypress/integration/examples/payment.js
+++ tests/integration-tests-1.6.1.x/cypress/integration/examples/payment.js
@@ -289,60 +289,59 @@ describe('Presta 1.6', function () {
         })
     })
     
-    //Todo: Uncomment when Ideal issue would resolve
-    // it('TC#9: iDeal Full Capture & Refund', function () {
+    it('TC#9: iDeal Full Capture & Refund', function () {
 
-    //     const ord = new Order()
-    //     ord.clrcookies()
-    //     ord.visit()
-    //     cy.get('body').then(($body) => {
-    //         if ($body.text().includes('€')) {
-    //             ord.addproduct()
-    //             cy.fixture('config').then((admin) => {
-    //                 if (admin.iDEAL_EUR_TERMINAL != "") {
-    //                     cy.get('body').then(($a) => {
-    //                         if ($a.find("a:contains('" + admin.iDEAL_EUR_TERMINAL + "')").length) {
-    //                             ord.ideal_payment(admin.iDEAL_EUR_TERMINAL)
-    //                             ord.admin()
-    //                             ord.ideal_refund()
-    //                         } else {
-    //                             cy.log(admin.iDEAL_EUR_TERMINAL + ' not found in page')
-    //                             this.skip()
-    //                         }
-    //                     })
-    //                 }
-    //                 else {
-    //                     cy.log('iDEAL_EUR_TERMINAL skipped')
-    //                     this.skip()
-    //                 }
-    //             })
-    //         }
-    //         else {
-    //             ord.admin()
-    //             ord.change_currency_to_EUR_for_iDEAL()
-    //             ord.clear_cache()
-    //             ord.re_save_EUR_currency_config()
-    //             ord.visit()
-    //             ord.addproduct()
-    //             cy.fixture('config').then((admin) => {
-    //                 if (admin.iDEAL_EUR_TERMINAL != "") {
-    //                     cy.get('body').then(($a) => {
-    //                         if ($a.find("a:contains('" + admin.iDEAL_EUR_TERMINAL + "')").length) {
-    //                             ord.ideal_payment(admin.iDEAL_EUR_TERMINAL)
-    //                             ord.admin()
-    //                             ord.ideal_refund()
-    //                         } else {
-    //                             cy.log(admin.iDEAL_EUR_TERMINAL + ' not found in page')
-    //                             this.skip()
-    //                         }
-    //                     })
-    //                 }
-    //                 else {
-    //                     cy.log('iDEAL_EUR_TERMINAL skipped')
-    //                     this.skip()
-    //                 }
-    //             })
-    //         }
-    //     })
-    // })
+        const ord = new Order()
+        ord.clrcookies()
+        ord.visit()
+        cy.get('body').then(($body) => {
+            if ($body.text().includes('€')) {
+                ord.addproduct()
+                cy.fixture('config').then((admin) => {
+                    if (admin.iDEAL_EUR_TERMINAL != "") {
+                        cy.get('body').then(($a) => {
+                            if ($a.find("a:contains('" + admin.iDEAL_EUR_TERMINAL + "')").length) {
+                                ord.ideal_payment(admin.iDEAL_EUR_TERMINAL)
+                                ord.admin()
+                                ord.ideal_refund()
+                            } else {
+                                cy.log(admin.iDEAL_EUR_TERMINAL + ' not found in page')
+                                this.skip()
+                            }
+                        })
+                    }
+                    else {
+                        cy.log('iDEAL_EUR_TERMINAL skipped')
+                        this.skip()
+                    }
+                })
+            }
+            else {
+                ord.admin()
+                ord.change_currency_to_EUR_for_iDEAL()
+                ord.clear_cache()
+                ord.re_save_EUR_currency_config()
+                ord.visit()
+                ord.addproduct()
+                cy.fixture('config').then((admin) => {
+                    if (admin.iDEAL_EUR_TERMINAL != "") {
+                        cy.get('body').then(($a) => {
+                            if ($a.find("a:contains('" + admin.iDEAL_EUR_TERMINAL + "')").length) {
+                                ord.ideal_payment(admin.iDEAL_EUR_TERMINAL)
+                                ord.admin()
+                                ord.ideal_refund()
+                            } else {
+                                cy.log(admin.iDEAL_EUR_TERMINAL + ' not found in page')
+                                this.skip()
+                            }
+                        })
+                    }
+                    else {
+                        cy.log('iDEAL_EUR_TERMINAL skipped')
+                        this.skip()
+                    }
+                })
+            }
+        })
+    })
 })
