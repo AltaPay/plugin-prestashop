ee983f751bf6025da16b04e7e6e15596b52f5fc7
Bushra Asif <bushraasif931@gmail.com>
2023-05-09 11:40:29 +0500

    Update changelog and release version

--- helpers.php
+++ helpers.php
@@ -24,7 +24,7 @@
 function transactionInfo($transactionInfo = [])
 {
     $pluginName = 'altapay';
-    $pluginVersion = '3.5.1';
+    $pluginVersion = '3.5.2';
 
     // Transaction info
     $transactionInfo['ecomPlatform'] = 'PrestaShop';
