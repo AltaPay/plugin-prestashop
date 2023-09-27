14bb835e9620e44d6bc3e152329e05d6b912d8bc
Bushra Asif <bushraasif931@gmail.com>
2023-09-27 09:59:07 +0500

    Fix phpcs issue

--- controllers/front/callbackredirect.php
+++ controllers/front/callbackredirect.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * AltaPay module for PrestaShop
  *
@@ -7,6 +6,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 class AltapayCallbackredirectModuleFrontController extends ModuleFrontController
 {
     /**
