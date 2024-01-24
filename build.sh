7f6c41a628a9fd7988ffc7426e9fa1e82a3fdaf7
Shahbaz <shahbaz.mehar@outlook.com>
2024-01-24 17:31:26 +0500

    Build with PHP version 7.0

--- build.sh
+++ build.sh
@@ -10,25 +10,18 @@ then
     exit
   fi
 
-  if ! command -v php7.2
-  then
-    echo "PHP 7.2 package is not currently installed"
-    exit
-  fi
-
   if ! command -v composer
   then
     echo "Composer package is not currently installed"
     exit
   fi
 
-  php7.2 $(command -v composer) install --no-dev --no-interaction
-  yes | php7.2 vendor/bin/php-scoper add-prefix
-  rsync -a build/vendor/* vendor/ && rm -rf build/
+  composer install --no-dev --no-interaction
+  composer isolate --no-interaction
+  composer dump -o --no-interaction
   find . -type d -exec cp index.php {} \;
-  php7.2 $(command -v composer) dump-autoload --working-dir ./ --classmap-authoritative
-  mkdir -p dist/altapay && rsync -av --exclude={'build','dist','docker','Docs','build.sh','guide.md','.gitignore','phpstan.neon','composer.json','composer.lock'} * dist/altapay
-  cd dist/altapay/ && cd ../ && zip altapay.zip -r *
+  mkdir -p dist/altapay && rsync -av --exclude={'dist','docker','Docs','build.sh','guide.md','.gitignore','phpstan.neon','composer.json','composer.lock'} * dist/altapay
+  cd dist/ && zip altapay-for-prestashop-1.6.x.zip -r *
 else
   echo "Zip package is not currently installed"
 fi
