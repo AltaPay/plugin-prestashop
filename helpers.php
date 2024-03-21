80627c8322d7172672bb8d04a26fbf399f3ebfde
Mahad Ashraf <>
2024-03-21 13:12:17 +0500

    Update version to 3.7.5

--- helpers.php
+++ helpers.php
@@ -24,7 +24,7 @@
 function transactionInfo($transactionInfo = [])
 {
     $pluginName = 'altapay';
-    $pluginVersion = '3.7.4';
+    $pluginVersion = '3.7.5';
 
     // Transaction info
     $transactionInfo['ecomPlatform'] = 'PrestaShop';
