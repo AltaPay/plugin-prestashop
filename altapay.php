1222662e20d6b88271ea08b99233da3db42ff679
Bushra Asif <bushraasif931@gmail.com>
2024-07-05 12:44:22 +0500

    Update version in altapay.php

--- altapay.php
+++ altapay.php
@@ -32,7 +32,7 @@ class ALTAPAY extends PaymentModule
     {
         $this->name = 'altapay';
         $this->tab = 'payments_gateways';
-        $this->version = '3.8.2';
+        $this->version = '3.8.3';
         $this->author = 'AltaPay A/S';
         $this->is_eu_compatible = 1;
         $this->ps_versions_compliancy = ['min' => '1.6.0.1', 'max' => '8.1.7'];
