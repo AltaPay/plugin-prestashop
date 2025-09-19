14829f81ba52f46ed477b835c7a8c940fbfa0e3c
Shahbaz <shahbaz.mehar@outlook.com>
2025-09-19 17:57:11 +0500

    Update max supported version to 8.2.3

--- altapay.php
+++ altapay.php
@@ -35,7 +35,7 @@ class ALTAPAY extends PaymentModule
         $this->version = '4.0.1';
         $this->author = 'AltaPay A/S';
         $this->is_eu_compatible = 1;
-        $this->ps_versions_compliancy = ['min' => '1.6.0.1', 'max' => '8.2.1'];
+        $this->ps_versions_compliancy = ['min' => '1.6.0.1', 'max' => '8.2.3'];
         $this->currencies = true;
         $this->currencies_mode = 'checkbox';
         $this->bootstrap = true;
